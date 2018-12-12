b27c0898eedbc10da31b6e5cbd6c6326f688f967
Tuuuuooo <emura1142@gmail.com>
2018-12-12 23:59:31 +0900

    Update index.*

--- index.js
+++ index.js
@@ -2,6 +2,32 @@ var canvas =
 	document.getElementById('canvas');
 var canvasWidth = 400;
 var canvasHeight = 300;
+var uploadImgSrc;
 
 // Canvasの準備
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+var ctx = canvas.getContext('2d');
+
+function loadLocalImage(e) {
+	//ファイル情報を取得
+	var fileData = e.target.files[0];
+	
+	//画像ファイル以外は処理を止める
+	if(!fileData.type.match('image.*')) {
+		alert('画像を選択してください');
+		return;
+	}
+	
+	//FileReaderオブジェクトを使ってファイル読み込み
+	var reader = new FileReader();
+	//ファイル読み込みに成功した時の処理
+	reader.onload = function() {
+	    //Canvas上に表示する
+	    uploadImgSrc = reader.result;
+	    canvasDraw();
+	}
+	//ファイル読み込みを実行
+	reader.readAsDataURL(fileData);
+}
 
